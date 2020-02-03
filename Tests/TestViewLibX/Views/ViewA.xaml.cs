a74c713c499a59cd9972a17959d9759a53bbb48d
Marek Grochowski <marekgrochowski@web.de>
2020-02-03 04:17:09 +0100

    Tests changed to netcore framework

---
+++ Tests/TestViewLibX/Views/ViewA.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace TestViewLib.Views
+{
+    /// <summary>
+    /// Interaction logic for ViewA.xaml
+    /// </summary>
+    public partial class ViewA : UserControl
+    {
+        #region Public Constructors
+
+        public ViewA()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Public Constructors
+    }
+}
