ecc7d3af8c9b80b54d1a2b6218c00ce382863ab2
Marek Grochowski <marekgrochowski@web.de>
2020-02-16 12:49:06 +0000

    Register files corrected

--- Helpers/_Register.bat
+++ Helpers/_Register.bat
@@ -2,3 +2,6 @@ pushd ..
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\TestAddIn1.dll
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn2\bin\Debug\net472\TestAddIn2.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn2\bin\Debug\net472\PrismTaskPanes.dll
