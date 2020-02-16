c5125dc06ef8b678b83b147caa18cbbbcf64fd64
Marek Grochowski <marekgrochowski@web.de>
2020-02-16 12:37:34 +0000

    Helper files re-located

---
+++ Helpers/_Register.bat
@@ -0,0 +1,4 @@
+pushd ..
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\TestAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\PrismTaskPanes.dll
