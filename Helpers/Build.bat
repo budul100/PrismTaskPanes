eabff9985c790bc60f8681a63627c85a07eaa78b
Marek Grochowski <marekgrochowski@web.de>
2020-05-06 02:46:07 +0200

    Build helper updated

--- Helpers/Build.bat
+++ Helpers/Build.bat
@@ -2,3 +2,4 @@ CALL Unregister.bat
 CALL Clean.bat
 
 dotnet build "..\PrismTaskPanes\PrismTaskPanes.csproj"  --configuration Release
+dotnet build "..\\PrismTaskPanes.Regions\PrismTaskPanes.Regions.csproj"  --configuration Release
