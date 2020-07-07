ad05ee7ead77cd8b950890a5f120b889e2056aca
Marek Grochowski <marekgrochowski@web.de>
2020-07-08 00:44:58 +0200

    Helpers updated

--- Helpers/Register.bat
+++ Helpers/Register.bat
@@ -1,12 +1,12 @@
 PUSHD ..
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\TestAddIn1.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn1\bin\Debug\net472\TestAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn1\bin\Debug\net472\PrismTaskPanes.dll
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn2\bin\Debug\net472\TestAddIn2.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn2\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn2\bin\Debug\net472\TestAddIn2.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn2\bin\Debug\net472\PrismTaskPanes.dll
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Release\net472\TestAddIn1.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\Tests\TestAddIn1\bin\Release\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn1\bin\Release\net472\TestAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb .\PrismTaskPanes.Tests\TestAddIn1\bin\Release\net472\PrismTaskPanes.dll
 
 POPD
