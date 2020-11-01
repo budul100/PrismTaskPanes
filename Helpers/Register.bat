c879f47aec7e57ec9a8db118a8bc16d63f3efe04
Marek Grochowski <marekgrochowski@web.de>
2020-11-01 18:31:34 +0100

    Code tydied

--- Helpers/Register.bat
+++ Helpers/Register.bat
@@ -1,13 +1,9 @@
 @ECHO off
 
-SET ExampleDirectory=.\Examples
-
-PUSHD ..
+SET ExampleDirectory=..\Examples
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
 
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
 "%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
-
-POPD
