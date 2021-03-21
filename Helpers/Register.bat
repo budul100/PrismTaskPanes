1dae5e7c22f58b12e3c02d0f6092352fc6203344
Marek Grochowski <marekgrochowski@web.de>
2021-03-21 23:09:17 +0100

    PowerPoint examples added

--- Helpers/Register.bat
+++ Helpers/Register.bat
@@ -1,9 +1,16 @@
 @ECHO off
 
-SET ExampleDirectory=..\Examples
+SET ExampleDirectoryExcel=..\Examples\Excel
+SET ExampleDirectoryPowerPoint=..\Examples\PowerPoint
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryExcel%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryExcel%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryExcel%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryExcel%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryPowerPoint%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryPowerPoint%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryPowerPoint%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /tlb %ExampleDirectoryPowerPoint%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
--- Helpers/Unregister.bat
+++ Helpers/Unregister.bat
@@ -1,9 +1,16 @@
 @ECHO off
 
-SET ExampleDirectory=..\Examples
+SET ExampleDirectoryExcel=..\Examples\Excel
+SET ExampleDirectoryPowerPoint=..\Examples\PowerPoint
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectory%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryExcel%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryExcel%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
-"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectory%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryExcel%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryExcel%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryPowerPoint%\ExampleAddIn1\bin\Debug\net472\ExampleAddIn1.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryPowerPoint%\ExampleAddIn1\bin\Debug\net472\PrismTaskPanes.dll
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryPowerPoint%\ExampleAddIn2\bin\Debug\net472\ExampleAddIn2.dll
+"%windir%\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe" /unregister %ExampleDirectoryPowerPoint%\ExampleAddIn2\bin\Debug\net472\PrismTaskPanes.dll
