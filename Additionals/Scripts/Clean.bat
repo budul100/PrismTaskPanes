a2a7c25c0dc9f06c3d0e1fcc634d2bb11830454b
budul <MarekGrochowski@web.de>
2022-06-28 19:15:37 +0200

    Version updated

--- Additionals/Scripts/Clean.bat
+++ Additionals/Scripts/Clean.bat
@@ -4,5 +4,6 @@ pushd ..\..
 
 for /f "usebackq delims=" %%d in (`"dir /ad/b/s | sort /R"`) do rd "%%d"
 
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S _Out') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
--- Build.bat
+++ Build.bat
@@ -4,7 +4,7 @@ SET AdditionalsDir=.\Additionals
 SET HelperScripts=%AdditionalsDir%\Scripts
 SET SetupScripts=%AdditionalsDir%\Setup
 SET NuGetDir=%AdditionalsDir%\NuGet
-SET ProjectPaths='.\DryIoc\DryIoc.Excel\PrismTaskPanes.DryIoc.Excel.csproj','.\DryIoc\DryIoc.PowerPoint\PrismTaskPanes.DryIoc.PowerPoint.csproj','.\Base\View\View.csproj'
+SET ProjectPaths='.\DryIoc\DryIoc.Excel\DryIoc.Excel.csproj','.\DryIoc\DryIoc.PowerPoint\DryIoc.PowerPoint.csproj','.\Base\View\View.csproj'
 
 echo.
 echo ##### Create PrismTaskPanes #####
@@ -49,7 +49,7 @@ echo Clean solution
 echo.
 
 CALL "%HelperScripts%\Unregister.bat"
-CALL "%HelperScripts%\Clean.bat"
+CALL "%HelperScripts%\CleanFolders.bat"
 
 GOTO BUILD
 
@@ -59,17 +59,9 @@ echo.
 echo Build solution
 echo.
 
-dotnet build ".\DryIoc\DryIoc.Excel\PrismTaskPanes.DryIoc.Excel.csproj" --configuration %CONFIGURATION%
-dotnet build ".\DryIoc\DryIoc.PowerPoint\PrismTaskPanes.DryIoc.PowerPoint.csproj" --configuration %CONFIGURATION%
-dotnet build ".\Base\PrismTaskPanes.Commons\PrismTaskPanes.Commons.csproj" --configuration %CONFIGURATION%
+dotnet build ".\PrismTaskPanes.sln" --configuration %CONFIGURATION%
 
 if %CONFIGURATION% == Debug (
-
-	dotnet build ".\Examples\DryIoc\Excel\ExcelAddIn1\ExcelAddIn1.csproj" --configuration %CONFIGURATION%
-	dotnet build ".\Examples\DryIoc\Excel\ExcelAddIn2\ExcelAddIn2.csproj" --configuration %CONFIGURATION%
-
-	dotnet build ".\Examples\DryIoc\PowerPoint\PowerPointAddIn1\PowerPointAddIn1.csproj" --configuration %CONFIGURATION%
-	dotnet build ".\Examples\DryIoc\PowerPoint\PowerPointAddIn2\PowerPointAddIn2.csproj" --configuration %CONFIGURATION%
 
 	echo.
 	CALL "%HelperScripts%\Register.bat"
@@ -85,8 +77,10 @@ if %CONFIGURATION% == Debug (
 	echo.
 	echo Copy NuGet packages
 	echo.
-
+	
+	mkdir %NuGetDir%
 	del %NuGetDir%\*.nupkg
+	
 	for /R %cd% %%f in (*.nupkg) do copy %%f %NuGetDir%\
 
 	echo.
